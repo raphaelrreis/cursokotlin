3c01faea060e1087c1cd7b4a8160c8d63020b13e
Raphael Reis <rapphha@gmail.com>
2021-02-18 01:08:38 -0300

    Argumentos Variaveis

--- src/main/kotlin/fundamentos/funcoes/VarArgs.kt
+++ src/main/kotlin/fundamentos/funcoes/VarArgs.kt
@@ -1,10 +1,11 @@
 package fundamentos.funcoes
 
-fun ordenar(vararg numeros: Int): IntArray {
+fun ordenar(vararg numeros: Int, a: Int): IntArray {
+    println(a)
     return numeros.sortedArray()
 }
 
 fun main(args: Array<String>) {
-    for (n in ordenar(28, 3, 451, 88, 931))
+    for (n in ordenar(28, 3, 451, 88, 931, a = 1))
         println("$n ")
 }
