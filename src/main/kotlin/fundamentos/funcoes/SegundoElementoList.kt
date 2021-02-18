4890c4954b4bbc0e1ad03c534034b5fe13cc9b97
Raphael Reis <rapphha@gmail.com>
2021-02-18 01:03:02 -0300

    Refactoring and rename generics

--- src/main/kotlin/fundamentos/funcoes/SegundoElementoList.kt
+++ src/main/kotlin/fundamentos/funcoes/SegundoElementoList.kt
@@ -1,6 +1,6 @@
 package fundamentos.funcoes
 
-fun <E> List<E>.secondOrNull(): E? = if (this.size >= 2) this.get(1) else null
+fun <Element> List<Element>.secondOrNull(): Element? = if (this.size >= 2) this.get(1) else null
 
 fun main(args: Array<String>) {
     var list = listOf("Joao", "Maria", "Pedro")
