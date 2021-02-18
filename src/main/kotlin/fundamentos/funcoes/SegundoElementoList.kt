22887eafb1113f0e84e2e9f0adb02c50fb9c5859
Raphael Reis <rapphha@gmail.com>
2021-02-18 00:51:25 -0300

    add method in existents class using generics

--- src/main/kotlin/fundamentos/funcoes/SegundoElementoList.kt
+++ src/main/kotlin/fundamentos/funcoes/SegundoElementoList.kt
@@ -1,2 +1,8 @@
 package fundamentos.funcoes
 
+fun <E> List<E>.secondOrNull(): E? = if (this.size >= 2) this.get(1) else null
+
+fun main(args: Array<String>) {
+    var list = listOf("Joao", "Maria", "Pedro")
+    println(list.secondOrNull())
+}
