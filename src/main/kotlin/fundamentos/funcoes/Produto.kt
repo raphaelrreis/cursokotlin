783c583d458741f2708f09ddc09d1235f2e7f00d
Raphael Reis <rapphha@gmail.com>
2021-02-19 18:22:26 -0300

    Utilizando funcao unit and refactoring unused args

--- src/main/kotlin/fundamentos/funcoes/Produto.kt
+++ src/main/kotlin/fundamentos/funcoes/Produto.kt
@@ -4,7 +4,7 @@ class Produto(val nome: String, val preco: Double)
 
 infix fun Produto.maisCaroQue(produto: Produto): Boolean = this.preco > produto.preco
 
-fun main(args: Array<String>) {
+fun main() {
     val p1 = Produto("IPAD",  preco  = 2348.00)
     val p2 = Produto(preco = 3.49, nome = "Borracha")
     println(p1 maisCaroQue p2)
---
+++ src/main/kotlin/fundamentos/funcoes/UnitReturn.kt
@@ -0,0 +1,20 @@
+package fundamentos.funcoes
+
+fun imprimeMaior(a: Int, b: Int) {
+    println(Math.max(a, b))
+    return Unit
+}
+
+fun imprimeMaior2(a: Int, b: Int): Unit {
+    println(Math.max(a, b))
+    return Unit
+}
+
+fun impeimrMeior5(a: Int, b: Int): Unit {
+    println(Math.max(a, b))
+    return Unit
+}
+
+fun main(args: Array<String>) {
+
+}
