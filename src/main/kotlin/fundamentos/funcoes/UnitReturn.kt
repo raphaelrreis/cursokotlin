eb86404db63b8b59ee545c913a67ac4e0b5b8ef4
Raphael Reis <rapphha@gmail.com>
2021-02-19 18:26:59 -0300

    create function main usando .run

--- src/main/kotlin/fundamentos/funcoes/UnitReturn.kt
+++ src/main/kotlin/fundamentos/funcoes/UnitReturn.kt
@@ -1,6 +1,6 @@
 package fundamentos.funcoes
 
-fun imprimeMaior(a: Int, b: Int) {
+fun imprimeMaior1(a: Int, b: Int) {
     println(Math.max(a, b))
     return Unit
 }
@@ -10,11 +10,21 @@ fun imprimeMaior2(a: Int, b: Int): Unit {
     return Unit
 }
 
-fun impeimrMeior5(a: Int, b: Int): Unit {
+fun imprimeMaior3(a: Int, b: Int) {
+    println(Math.max(a, b))
+    return Unit
+}
+
+fun imprimrMeior5(a: Int, b: Int): Unit {
     println(Math.max(a, b))
     return Unit
 }
 
 fun main(args: Array<String>) {
+    imprimeMaior1(2, 1)
+    imprimeMaior2(2, 1)
+    imprimeMaior3(1, 2)
+    imprimrMeior5(2, 1).run { 2 > 4 }.run { 3 > 4 }.run { 4 > 5 }
+
 
 }
