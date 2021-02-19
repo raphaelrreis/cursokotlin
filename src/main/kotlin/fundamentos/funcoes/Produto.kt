d0d36e39f4503f7db579279a2e360638f9aebda4
Raphael Reis <rapphha@gmail.com>
2021-02-19 18:14:59 -0300

    Utilizando funcao infix

---
+++ src/main/kotlin/fundamentos/funcoes/Produto.kt
@@ -0,0 +1,11 @@
+package fundamentos.funcoes
+
+class Produto(val nome: String, val preco: Double)
+
+infix fun Produto.maisCaroQue(produto: Produto): Boolean = this.preco > produto.preco
+
+fun main(args: Array<String>) {
+    val p1 = Produto("IPAD",  preco  = 2348.00)
+    val p2 = Produto(preco = 3.49, nome = "Borracha")
+    println(p1 maisCaroQue p2)
+}
